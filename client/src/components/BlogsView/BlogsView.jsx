a66c0c9a580b250fb700a866826462b30e3a120a
SeiamAlMahmud <samahmud49@gmail.com>
2024-10-02 06:06:03 +0000

    add aos effect

--- client/src/components/BlogsView/BlogsView.jsx
+++ client/src/components/BlogsView/BlogsView.jsx
@@ -1,20 +1,33 @@
 
+import { useEffect } from 'react';
 import ActionAreaCard  from '../Homepage/ActionAreaCard/ActionAreaCard'; // Import the Card component
 import { ListWithIcon } from '../Homepage/ListWithIcon/ListWithIcon';
 
+import AOS from 'aos';
+import 'aos/dist/aos.css'; // Make sure to import the AOS styles
+
+
 export default function BlogsView() {
+
+  useEffect(() => {
+    AOS.init({
+      duration: 300,
+      easing: 'ease-in-sine',
+      delay: 100,
+    });
+  }, []); // Empty dependency array ensures AOS is initialized only once when the component mounts
   return (
     <>
     <div className='flex flex-col xl:flex-row justify-center items-start my-5 gap-6 mx-auto'>
         <div>
             <h2 className='text-left my-3 text-xl font-bold border-b-2 border-b-pink-400 w-52 hover:w-72 transition-all mb-5'>News</h2>
-        <div className='flex-wrap flex mx-auto gap-6 justify-center xl:flex-nowrap'>
+        <div className='flex-wrap flex mx-auto gap-6 justify-center xl:flex-nowrap' data-aos="fade-up">
             <ActionAreaCard />
             <ActionAreaCard />
             <ActionAreaCard />
         </div>
         </div>
-        <div className='flex flex-col mx-auto w-[90%] xl:w-1/2'>
+        <div className='flex flex-col mx-auto w-[90%] xl:w-1/2' data-aos="zoom-in">
         <h2 className='text-left my-3 text-xl font-bold border-b-2 border-b-pink-400 mb-6 pb-1'>Notice</h2>
         <ListWithIcon />
         </div>
--- client/src/pages/Homepage.jsx
+++ client/src/pages/Homepage.jsx
@@ -1,14 +1,17 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { CarouselCustomArrows } from '../components/Carousol/CarouselCustomArrows'
 import BlogsView from '../components/BlogsView/BlogsView'
 
 const Homepage = () => {
+  
+ 
+
   return (
     <>
     <div className='max-w-screen-xl mx-auto mt-4'>
         <CarouselCustomArrows />
     </div>
-    <div className='max-w-screen-2xl'>
+    <div className='max-w-screen-2xl' >
         <BlogsView />
     </div>
     </>
