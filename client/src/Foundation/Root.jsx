03ca98677bf6cd8a96eb22485ce4e06da158cb49
SeiamAlMahmud <samahmud49@gmail.com>
2024-10-02 13:52:31 +0000

    Footer

--- client/src/Foundation/Root.jsx
+++ client/src/Foundation/Root.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import { NavbarWithMegaMenu } from '../components/NavbarWithMegaMenu/NavbarWithMegaMenu'
 import { Outlet } from 'react-router-dom'
+import Footer from '../components/Footer/Footer'
 
 const Root = () => {
   return (
     <div>
         <NavbarWithMegaMenu />
         <Outlet/>
+        <Footer />
     </div>
   )
 }
